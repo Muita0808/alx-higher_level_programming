dc58cb3a7d8f064646a30c2895392ec62a7ce898
Muita0808 <magdalinemuita@gmail.com>
2023-12-04 22:03:22 -0800

    new

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+for char_code in range(ord('a'), ord('z') + 1):
+    if chr(char_code) not in ['q', 'e']:
+        print("{:c}".format(char_code), end="")
